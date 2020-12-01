9ede3a89c4ceccb4bf4e94a2da47b54f637c8e82
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-12-01 13:52:30 +0100

    1: Second part

--- 1.cpp
+++ 1.cpp
@@ -1,8 +1,11 @@
-// Complexity : O(N log N)
+// 1 Complexity : O(N log N)
+// 2 Complexity : O(N^2 log N)
 
 #include <bits/stdc++.h>
 
 using namespace std;
+
+#define PART2
 
 int main() {
     // Number of entries in the input file
@@ -14,11 +17,22 @@ int main() {
 
     sort(inputs.begin(), inputs.end());
 
+#ifdef PART1
     for (int i = 0; i < N; ++i)
         if (binary_search(inputs.begin(), inputs.end(), 2020 - inputs[i])) {
             cout << (2020 - inputs[i]) * inputs[i] << endl;
-            break;
+            return 0;
         }
+#else
+    for (int i = 0; i < N; ++i)
+        for (int j = i + 1; j < N; ++j)
+            if (binary_search(inputs.begin(), inputs.end(), 2020 - inputs[i]
+                        - inputs[j])) {
+                cout << (2020 - inputs[i] - inputs[j]) * inputs[i] * inputs[j]
+                    << endl;
+                return 0;
+            }
+#endif
 
-    return 0;
+    return -1;
 }
