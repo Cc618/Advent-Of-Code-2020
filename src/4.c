a7ad4074b55fe9ff7fad5d40834d0ce8aaa8d8e8
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-12-04 10:42:16 +0100

    4: Started part 2

--- src/4.c
+++ src/4.c
@@ -2,13 +2,37 @@
 #include <string.h>
 #include <stddef.h>
 
+// Uncomment for part 1
 // #define PART1
 
 // Max entry size
 char buf[4096];
 
+static int byrValid(char *val) {
+
+}
+
+static int iyrValid(char *val) {
+}
+
+static int eyrValid(char *val) {
+}
+
+static int hgtValid(char *val) {
+}
+
+static int hclValid(char *val) {
+}
+
+static int eclValid(char *val) {
+}
+
+static int pidValid(char *val) {
+}
+
 // Token valid
 int isvalid(char *name, char *val) {
+#ifdef PART1
     return  strcmp(name, "byr") == 0 ||
             strcmp(name, "iyr") == 0 ||
             strcmp(name, "eyr") == 0 ||
@@ -16,6 +40,27 @@ int isvalid(char *name, char *val) {
             strcmp(name, "hcl") == 0 ||
             strcmp(name, "ecl") == 0 ||
             strcmp(name, "pid") == 0;
+#else
+    struct {
+        const char *name;
+        int (*validator)(char *val);
+    } entries[] = {
+        { "byr", byrValid },
+        { "iyr", iyrValid },
+        { "eyr", eyrValid },
+        { "hgt", hgtValid },
+        { "hcl", hclValid },
+        { "ecl", eclValid },
+        { "pid", pidValid },
+    };
+
+    for (int i = 0; i < 7; ++i) {
+        if (strcmp(entries[i].name, name) == 0)
+            return 1;
+    }
+
+    return 0;
+#endif
 }
 
 int check(char *buf, size_t len) {
@@ -56,7 +101,11 @@ int main() {
             buf[bufi++] = c;
     } while (1);
 
+#ifdef PART1
     printf("Part 1: %zu\n", valid);
+#else
+    printf("Part 2: %zu\n", valid);
+#endif
 
     return 0;
 }
