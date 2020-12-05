a9a9a7721e16d1f2cc686bb6e74ef361c85447cf
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-12-05 09:26:16 +0100

    5: Added C part 1

---
+++ src/5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stddef.h>
+
+int getPos(const char *val, int len, int high, char upper) {
+    int low = 0;
+    for (int i = 0; i < len; ++i) {
+        int mid = (high + low) / 2;
+        if (val[i] == upper)
+            low = mid + 1;
+        else
+            high = mid;
+    }
+
+    return low;
+}
+
+int getId(const char *val) {
+    int row = getPos(val, 7, 127, 'B');
+    int col = getPos(val + 7, 3, 7, 'R');
+
+    return row * 8 + col;
+}
+
+int main() {
+    int mx = 0;
+
+    char buf[16];
+    do {
+        fgets(buf, 16, stdin);
+
+        if (feof(stdin))
+            break;
+
+        int id = getId(buf);
+
+        mx = id > mx ? id : mx;
+    } while (1);
+
+    printf("Part 1 : %d\n", mx);
+
+    return 0;
+}
