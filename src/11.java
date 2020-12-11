a54f0a49bdc61709735912ff55eb0001327a5268
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-12-11 10:03:41 +0100

    11: Added 2nd part

--- src/11.java
+++ src/11.java
@@ -2,8 +2,28 @@ import java.util.*;
 import java.io.*;
 
 class Problem {
+    // Returns whether a seat in this direction is occupied
+    // (Part 2 only)
+    private static int isOccup(ArrayList<char[]> grid, int rows, int cols, int i, int j, int dx, int dy) {
+        while (true) {
+            i += dy;
+            j += dx;
+
+            if (i < 0 || j < 0 || i >= rows || j >= cols) break;
+
+            if (grid.get(i)[j] == '#')
+                return 1;
+            if (grid.get(i)[j] == 'L')
+                return 0;
+        }
+
+        return 0;
+    }
+
     public static void main(String[] args) {
         try {
+            boolean part1 = false;
+
             ArrayList<char[]> grid1 = new ArrayList<>();
             ArrayList<char[]> grid2 = new ArrayList<>();
 
@@ -32,26 +52,48 @@ class Problem {
                 // O(N^2)
                 for (int i = 0; i < rows; ++i) {
                     for (int j = 0; j < cols; ++j) {
-                        // Count occupied seats
                         int nOccup = 0;
-                        for (int dx = 0; dx < 3; ++dx) {
-                            for (int dy = 0; dy < 3; ++dy) {
-                                if (!(dx == 1 && dy == 1) &&
-                                        i - 1 + dy >= 0 && i - 1 + dy < rows &&
-                                        j - 1 + dx >= 0 && j - 1 + dx < cols) {
-                                    if (first.get(i - 1 + dy)[j - 1 + dx] == '#')
-                                        ++nOccup;
-                                    }
-                            }
-                        }
 
-                        if (first.get(i)[j] == 'L') {
-                            second.get(i)[j] = nOccup == 0 ? '#' : 'L';
-                        }
-                        else if (first.get(i)[j] == '#') {
-                            second.get(i)[j] = nOccup < 4 ? '#' : 'L';
+                        if (part1) {
+                            // Count occupied seats
+                            for (int dx = 0; dx < 3; ++dx) {
+                                for (int dy = 0; dy < 3; ++dy) {
+                                    if (!(dx == 1 && dy == 1) &&
+                                            i - 1 + dy >= 0 && i - 1 + dy < rows &&
+                                            j - 1 + dx >= 0 && j - 1 + dx < cols) {
+                                        if (first.get(i - 1 + dy)[j - 1 + dx] == '#')
+                                            ++nOccup;
+                                        }
+                                }
+                            }
+
+                            if (first.get(i)[j] == 'L') {
+                                second.get(i)[j] = nOccup == 0 ? '#' : 'L';
+                            }
+                            else if (first.get(i)[j] == '#') {
+                                second.get(i)[j] = nOccup < 4 ? '#' : 'L';
+                            } else {
+                                second.get(i)[j] = '.';
+                            }
                         } else {
-                            second.get(i)[j] = '.';
+                            // O(N)
+                            nOccup += isOccup(first, rows, cols, i, j, 0, 1);
+                            nOccup += isOccup(first, rows, cols, i, j, 0, -1);
+                            nOccup += isOccup(first, rows, cols, i, j, 1, 0);
+                            nOccup += isOccup(first, rows, cols, i, j, -1, 0);
+                            nOccup += isOccup(first, rows, cols, i, j, 1, 1);
+                            nOccup += isOccup(first, rows, cols, i, j, -1, -1);
+                            nOccup += isOccup(first, rows, cols, i, j, 1, -1);
+                            nOccup += isOccup(first, rows, cols, i, j, -1, 1);
+
+                            if (first.get(i)[j] == 'L') {
+                                second.get(i)[j] = nOccup == 0 ? '#' : 'L';
+                            }
+                            else if (first.get(i)[j] == '#') {
+                                second.get(i)[j] = nOccup < 5 ? '#' : 'L';
+                            } else {
+                                second.get(i)[j] = '.';
+                            }
                         }
 
                         changes |= second.get(i)[j] != first.get(i)[j];
@@ -76,7 +118,7 @@ class Problem {
                     if (grid1.get(i)[j] == '#')
                         ++occup;
 
-            System.out.print("Part 1: ");
+            System.out.print(part1 ? "Part 1: " : "Part 2: ");
             System.out.println(occup);
         } catch (Exception e) {
             System.err.println(e);
