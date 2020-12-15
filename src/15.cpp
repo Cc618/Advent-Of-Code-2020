92ee1525c4d6cbe1902356644db0617702445e6e
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-12-15 13:19:51 +0100

    15: Added part 2

--- src/15.cpp
+++ src/15.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+// For part 1
+// #define PART1 1
+
 int main() {
     // Returns the index of the last occurence of the number
     unordered_map<int, int> lastPos;
@@ -23,8 +26,13 @@ int main() {
     // We suppose no duplicates, the last one is not yet for searching
     lastPos.erase(nb.back());
 
+    // O(1) time (not dependant of the input), O(N) memory
     int next;
+#ifdef PART1
     for (int i = nb.size(); i < 2020; ++i) {
+#else
+    for (int i = nb.size(); i < 30'000'000; ++i) {
+#endif
         auto search = lastPos.find(current);
         if (search == lastPos.end()) {
             // New val
@@ -38,7 +46,11 @@ int main() {
         current = next;
     }
 
+#ifdef PART1
     cout << "Part 1 : " << current << endl;
+#else
+    cout << "Part 2 : " << current << endl;
+#endif
 
     return 0;
 }
